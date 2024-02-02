9e18bd3bdd171b8760c32d0585434c38c7b17567
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2024-02-02 13:12:03 +0100

    Setup server, add initial contact

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+
+let contacts = [
+    {
+        id: '1',
+        name: 'Dafina'
+    }
+];
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
